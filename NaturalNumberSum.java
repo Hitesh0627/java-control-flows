bebdaa144cd316555bf62d99837d072a7ee61083
Hitesh0627 <103327522+Hitesh0627@users.noreply.github.com>
2025-03-01 15:16:52 +0530

    [Hitesh] ADD: program to check if a number is a natural number and calculate the sum of n natural numbers

---
+++ NaturalNumberSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NaturalNumberSum{
+	public static void main(String [] args){
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter the Number: ");
+		int number = scanner.nextInt();
+
+		if(number >= 1){
+
+			int sum = number * (number + 1)/2;
+			System.out.println("The sum of " + number + " natural numbers is " + sum);
+		}
+		else{
+			System.out.println("The Sum of " + number + " is not natural number");
+		}
+		
+		scanner.close();
+	}
+}	
